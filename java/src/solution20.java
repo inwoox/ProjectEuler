2ddcfc0c3816a5362d6486eaf0e3726e5050b8a1
inwoo <inwoox@naver.com>
2021-08-06 20:41:26 +0900

    java - solution20

--- java/src/solution20.java
+++ java/src/solution20.java
@@ -1,2 +1,25 @@
+import java.math.BigInteger;
+
 public class solution20 {
+
+    public static BigInteger result = new BigInteger("1");
+    public static BigInteger one = new BigInteger("1");
+    public static void main(String[] args){
+        int sum = 0;
+        String[] strArr;
+        BigInteger num = new BigInteger("100");
+        result = jarisu(num);
+        strArr = result.toString().split("");
+        for (String s : strArr){
+            sum += Integer.parseInt(s);
+        }
+
+        System.out.println(sum);
+    }
+
+    public static BigInteger jarisu(BigInteger n){
+        result = result.multiply(n);
+        if (!n.equals(one)) return jarisu(n.subtract(one));
+        else return result;
+    }
 }
